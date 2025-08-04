0901e28e0bb9603dbce0777165e30f4e14462911
Nathan Saritzky <nsaritzky@Nathans-MacBook-Pro.local>
2025-08-04 06:57:07 -0700

    Support multiple PINGs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![allow(unused_imports)]
 use std::net::TcpListener;
-use std::io::Write;
+use std::io::{Read, Write};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
@@ -8,7 +8,14 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                stream.write_all(b"+PONG\r\n").unwrap();
+                let mut buf = Vec::<u8>::new();
+                stream.read_to_end(&mut buf).expect("Stream should read successfully");
+                if let Ok(commands) = String::from_utf8(buf) {
+                    for _ in commands.split_whitespace()
+                    {
+                        stream.write_all(b"+PONG\r\n").unwrap();
+                    }
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
