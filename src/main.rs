bd45c352ca5db29018109a194ae6a26939a9ae72
Nathan Saritzky <nsaritzky@Nathans-MacBook-Pro.local>
2025-08-05 16:47:28 -0700

    Implement parser and respond to ECHO

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,66 @@
 #![allow(unused_imports)]
+mod parser;
+
+use crate::parser::*;
+use anyhow::anyhow;
+use bytes::BytesMut;
 use tokio::{
     io::{self, AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpStream},
 };
 
 #[tokio::main]
-async fn main() -> io::Result<()> {
+async fn main() -> anyhow::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
     loop {
         let (socket, _) = listener.accept().await.unwrap();
 
         tokio::spawn(async move {
-            process(socket).await;
+            process(socket).await.expect("Process should be successful");
         });
     }
 }
 
-async fn process(mut socket: TcpStream) {
-    let mut buf = [0; 1024];
+async fn process(mut socket: TcpStream) -> anyhow::Result<()> {
+    let mut buf = BytesMut::with_capacity(4096);
 
     loop {
-        match socket.read(&mut buf).await {
-            Ok(0) => {
-                break
-            },
+        match socket.read_buf(&mut buf).await {
+            Ok(0) => break,
             Ok(n) => {
-                let input = String::from_utf8_lossy(&buf[..n]);
-                for _ in input.rmatches("PING") {
-                    socket.write_all(b"+PONG\r\n").await.expect("Should be able to write PONG");
-                    socket.flush().await.unwrap();
+                let results =
+                    parse_value(&mut &buf[..n]).map_err(|e| anyhow!("Failed to parse: {e}"))?;
+                if let ParseResult::Arr(v) = results {
+                    let mut i = 0;
+                    while i < v.len() {
+                        if let ParseResult::Primitive(PrimitiveParseResult::Str(s)) = &v[i] {
+                            if s.to_ascii_uppercase() == "PING" {
+                                socket.write_all(b"+PONG\r\n").await?;
+                                socket.flush().await?;
+                            } else if s.to_ascii_uppercase() == "ECHO" {
+                                i += 1;
+                                if let ParseResult::Primitive(PrimitiveParseResult::Str(resp)) =
+                                    &v[i]
+                                {
+                                    socket.write_all(&bulk_string(resp)).await?;
+                                    socket.flush().await?
+                                }
+                            }
+                        }
+                        i += 1;
+                    }
                 }
 
-                buf = [0; 1024];
+                buf.clear();
             }
             Err(_) => break,
         }
     }
+
+    Ok(())
+}
+
+fn bulk_string<'a>(s: &'a str) -> Vec<u8> {
+    format!("${}\r\n{s}\r\n", s.len()).as_bytes().to_owned()
 }
