27a09a001b2a0db4e9faabb97fd41b38d0d5e3ef
Nathan Saritzky <nsaritzky@Nathans-MacBook-Pro.local>
2025-08-07 13:58:05 -0700

    Send actually complete handshake

--- src/main.rs
+++ src/main.rs
@@ -230,6 +230,9 @@ async fn send_handshake() -> anyhow::Result<()> {
         .write_all(b"*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n")
         .await?;
     expect_response(&mut stream, b"+OK\r\n").await?;
+    stream
+        .write_all(b"*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n")
+        .await?;
 
     Ok(())
 }
