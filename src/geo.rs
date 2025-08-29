4d8919ab12e5250615dde4d3bb79bc2e81477f9f
Nathan Saritzky <nsaritzky@Nathans-MacBook-Pro.local>
2025-08-29 09:07:07 -0700

    Add geo item to zset

--- src/geo.rs
+++ src/geo.rs
@@ -1,6 +1,9 @@
 use crate::command_handler::CommandHandler;
+use crate::db_item::DbItem;
+use crate::db_value::DbValue;
 use crate::error::RedisError;
 use crate::response::Response;
+use crate::sorted_set::SortedSet;
 use crate::state::{ConnectionState, ServerState};
 use async_trait::async_trait;
 use bytes::Bytes;
@@ -12,15 +15,26 @@ impl CommandHandler for GeoAddHandler {
     async fn execute(
         &self,
         args: Vec<String>,
-        _server_state: ServerState,
+        mut server_state: ServerState,
         _connection_state: ConnectionState,
         _message_len: usize,
     ) -> Result<Vec<Bytes>, RedisError> {
         if args.len() != 5 {
             return Err(RedisError::WrongArgs("GEOADD"));
         }
-        let [_command, _key, long, lat, _member] = args.try_into().unwrap();
+        let [_command, key, long, lat, member] = args.try_into().unwrap();
         let (lat, long) = parse_coordinates(&lat, &long)?;
+
+        let mut item = server_state
+            .db
+            .get_mut_or_insert(key, DbItem::new(DbValue::ZSet(SortedSet::new()), None))
+            .await;
+        match item.value_mut() {
+            DbValue::ZSet(zset) => {
+                zset.insert_or_update(member, 0.0);
+            }
+            _ => return Err(RedisError::WrongType)
+        }
         Ok(vec![Response::Int(1).to_bytes()])
     }
 }
@@ -28,9 +42,7 @@ impl CommandHandler for GeoAddHandler {
 fn parse_coordinates(lat: &str, long: &str) -> Result<(f64, f64), GeoError> {
     let lat_num = lat.parse::<f64>();
     let long_num = long.parse::<f64>();
-    if lat_num.is_err()
-        || long_num.is_err()
-    {
+    if lat_num.is_err() || long_num.is_err() {
         Err(GeoError::InvalidLatLong(lat.to_string(), long.to_string()))
     } else {
         let lat = lat_num.unwrap();
